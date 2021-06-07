d5841c8e921a2bf1bb9b76aeaf2c24fd3753a41c
Teemo Vaas <teemo@digiwood.ee>
2021-06-07 23:27:13 +0300

    Minor type conversion optimization in speeduino.ino
    Avoid converting uint16_t to long and back. Hopefully saves some instruction cycles.

--- speeduino/speeduino.ino
+++ speeduino/speeduino.ino
@@ -151,8 +151,8 @@ void loop()
     unsigned long timeToLastTooth = (currentLoopTime - toothLastToothTime);
     if ( (timeToLastTooth < MAX_STALL_TIME) || (toothLastToothTime > currentLoopTime) ) //Check how long ago the last tooth was seen compared to now. If it was more than half a second ago then the engine is probably stopped. toothLastToothTime can be greater than currentLoopTime if a pulse occurs between getting the lastest time and doing the comparison
     {
-      currentStatus.longRPM = getRPM(); //Long RPM is included here
-      currentStatus.RPM = currentStatus.longRPM;
+      currentStatus.RPM = getRPM(); 
+      currentStatus.longRPM = currentStatus.RPM;//Long RPM is included here
       currentStatus.RPMdiv100 = currentStatus.RPM / 100;
       FUEL_PUMP_ON();
       currentStatus.fuelPumpOn = true; //Not sure if this is needed.
