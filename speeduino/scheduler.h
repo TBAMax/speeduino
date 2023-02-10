359b986a414681f511577e323b79f8ad805ac7af
Teemo Vaas <36799067+TBAMax@users.noreply.github.com>
2023-02-10 19:19:39 +0200

    Remove outdated comment

--- speeduino/scheduler.h
+++ speeduino/scheduler.h
@@ -134,7 +134,6 @@ struct Schedule {
   void (*StartFunction)();        ///< Start Callback function for schedule
   void (*EndFunction)();          ///< End Callback function for schedule
 
-  //pure virtual functions, these are really defined in sub-classes
   COMPARE_TYPE getCounter(void) { return counter; }; //Function for getting counter value
   void setCompare(COMPARE_TYPE compareValue) { compare=(COMPARE_TYPE)compareValue; }; //Function for setting counter compare value
   void timerDisable(void) { pTimerEndFunction(); }; //Function to disable timer for specific channel
